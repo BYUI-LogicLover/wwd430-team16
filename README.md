cbcb3861ba5046a152c9294cec46320525081141
g10albert <albertmiguelg10@gmail.com>
2026-05-22 00:20:30 -0300

    Finished the setup to start our project. started the landing page file, started the css

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # wwd430-team16
+
+Travis Jay Nelson
+Albert Garcia
+Walter Munoa Sosa
+
+Color scheme:
+
+Backgrounds: #F8F8F8
+Highlights: #28582E
+Buttons/actions: #343434
+
+Typography:
+
+Titles: Montserrat
+Texts: Hind
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Hind } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const hind = Hind({
+  variable: "--font-hind",
+  subsets: ['latin'],
+  weight: ['300','400','500','600','700']
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${montserrat.variable, hind.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
